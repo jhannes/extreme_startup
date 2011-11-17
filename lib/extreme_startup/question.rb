0b7881b2a539d97314393d95cec950a682661915
Johannes Brodwall <johannes@brodwall.com>
2011-11-17 12:40:20 +0100

    Patch from Andrei Solntsev to avoid crashing on Cyrillic characters in response.

--- lib/extreme_startup/question.rb
+++ lib/extreme_startup/question.rb
@@ -70,7 +70,9 @@ module ExtremeStartup
     end
     
     def log_result
-      "|question: #{self.to_s}|answer: #{answer && answer[0..100].gsub("\n", "")}|expected: #{correct_answer}|result: #{result}"
+      "|question: #{self.to_s}|answer: #{answer[0..100].gsub("\n", "")}|expected: #{correct_answer}|result: #{result}"
+      rescue => e
+        "error during logging #{e}"
     end
 
     def id
