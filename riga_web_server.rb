53bcca0633565b7f85e51d92070335b20e2a4f8a
Johannes Brodwall <johannes@brodwall.com>
2012-04-03 11:14:48 +0200

    Example workshop implementation

---
+++ riga_web_server.rb
@@ -0,0 +1,22 @@
+﻿require 'rubygems'
+require_relative 'lib/extreme_startup/web_server'
+include ExtremeStartup
+include ExtremeStartup::Questions
+
+
+warmup_round = [WarmupQuestion]
+simple_round = [AdditionQuestion,MaximumQuestion,RememberMeQuestion,GeneralKnowledgeQuestion]
+medium_round = [MultiplicationQuestion, PrimesQuestion, SubtractionQuestion, DivisionQuestion]
+advanced_round = [WebshopQuestion, SquareCubeQuestion]
+hard_round   = [WebshopQuestion] * 2 + [FibonacciQuestion, AdditionMultiplicationQuestion, MultiplicationAdditionQuestion,AdditionAdditionQuestion]
+
+
+WebServer.settings.default_question_delay = 1
+WebServer.settings.question_factory = ExtremeStartup::GatedQuestionFactory.new([
+  warmup_round,
+  simple_round,
+  simple_round + medium_round,
+  medium_round + advanced_round,
+  medium_round + advanced_round + hard_round
+])
+WebServer.run!
