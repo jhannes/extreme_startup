5d955384dc63aae2e08d48c1501ee7cba2f9f0fd
Johannes Brodwall <johannes@brodwall.com>
2012-05-04 19:12:48 +0200

    Examples of workshops

---
+++ riga_web_server_warmup.rb
@@ -0,0 +1,16 @@
+﻿require 'rubygems'
+require_relative 'lib/extreme_startup/web_server'
+include ExtremeStartup
+include ExtremeStartup::Questions
+
+warmup_round = [WarmupQuestion]
+warmup2_round = [AdditionQuestion]
+simple_round = [AdditionQuestion,MaximumQuestion,RememberMeQuestion,GeneralKnowledgeQuestion]
+
+WebServer.settings.default_question_delay = 5
+WebServer.settings.question_factory = ExtremeStartup::GatedQuestionFactory.new([
+  warmup_round,
+  warmup2_round,
+  simple_round,
+])
+WebServer.run!
